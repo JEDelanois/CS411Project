c538a0df1a5a8063a185d64fb86d317ee86fe24f
Faisal <faisal.abujabal@gmail.com>
2016-03-17 02:51:42 -0500

    some changes to database settings

--- data.py
+++ data.py
@@ -6,7 +6,10 @@ from credentials import host
 import shlex
 
 
-db = mdb.connect(host=host(), user=user(), passwd=password(), db=database() )
+# db = mdb.connect(host=host(), user=user(), passwd=password(), db=database() )
+db = mdb.connect(host="localhost",user="abujaba2_cs411us",passwd=".v,5istf~91H",db="abujaba2_CS411Project")
+# db = mdb.connect(host="localhost",user="root",passwd="root",db="cs411project")
+# db= _mysql.connect(host="localhost",user="joebob", passwd="moonpie",db="thangs")
 
 output = open('output.txt','r')
 
@@ -26,8 +29,9 @@ for i in range(0,100):
 
 	#repalce words with '_' witht he actual name from data base
 	#no urls were recorded so they should just be set to their default values right?
-	add_statement = 'INSERT INTO _TABLENAME (_PROTEIN, _FAT, _CARB, _SUGAR, _name) VALUES (%s,%s,%s,%s);'
-    cur.execute(add_statement, data_list )
+	# add_statement = 'INSERT INTO _TABLENAME (_PROTEIN, _FAT, _CARB, _SUGAR, _name) VALUES (%s,%s,%s,%s);'
+        add_statement = "INSERT INTO Ingredients (ingredient_protien, ingredient_fat, ingredient_fat, ingredient_fat, ingredient_name) VALUES (%s,%s,%s,%s, %s);"
+        cur.execute(add_statement, data_list)
 
 
   
