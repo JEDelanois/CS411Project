51d486d2a8db8c747a5ca75cf992600d5458c92e
Faisal <faisal.abujabal@gmail.com>
2016-03-25 03:20:07 -0500

    limited the api result to 30 elements

--- api/ingredients.php
+++ api/ingredients.php
@@ -7,6 +7,7 @@ $db = new DatabaseConnection();
 
 $ingredients = [];
 $results = $db->getAllIngredients();
+$results = array_slice($results, 0, 30);
 $ingredients['num_results'] = count($results);
 $ingredients['results'] = $results;
 
