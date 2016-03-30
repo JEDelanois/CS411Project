ccad89b8415dc0eb571956bc643b0497cafa8cdc
Faisal <faisal.abujabal@gmail.com>
2016-03-29 19:16:45 -0500

    added a registration endpoint

--- api/index.php
+++ api/index.php
@@ -1,1 +1,15 @@
 <a href="ingredients.php">Ingredients API</a>
+<p>
+<h2>Endpoint Usage:</h2>
+http://abujaba2.web.engr.illinois.edu/cs411project/api/ingredients.php?page=1
+<h2>Returns</h2>
+Dictionary containing the number of the results and an array of dictionaries that contain the results.
+</p>
+<hr>
+<a href="registerUser.php">Register User</a>
+<p>
+<h2>Endpoint Usage:</h2>
+http://abujaba2.web.engr.illinois.edu/cs411project/api/registerUser.php?firstname=David&&lastname=John&&email=david@john.com&&password=123
+<h2>Returns:</h2>
+On success returns the newely created user. On faliure returns null and an array of errors
+</p>
