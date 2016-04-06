f8ec8b440c6511059707b5b9cc32c9fdba089bd4
JEDelanois <jedelanois@gmail.com>
2016-04-06 17:58:24 -0500

    took out mysql stuff

--- dataCrawler/ingredientsDataCrawler/main.py
+++ dataCrawler/ingredientsDataCrawler/main.py
@@ -2,15 +2,15 @@ from nutritionvalueCrawler import CrawlPage
 from nutritionvalueCrawler import InitFile
 from nutritionvalueCrawler import GatherFoodOnPage
 from nutritionvalueCrawler import CrawlNutrionValue
-import MySQLdb as mdb
-from credentials import user
-from credentials import password
-from credentials import database
-from credentials import host
+#import MySQLdb as mdb
+#from credentials import user
+#from credentials import password
+#from credentials import database
+#from credentials import host
 
 
 f = open('output.txt', 'w')
-db = mdb.connect(host=host(), user=user(), passwd=password(), db=database() );
+db = 1# mdb.connect(host=host(), user=user(), passwd=password(), db=database() );
 
 CrawlNutrionValue(f, db)
 
--- dataCrawler/ingredientsDataCrawler/nutritionvalueCrawler.py
+++ dataCrawler/ingredientsDataCrawler/nutritionvalueCrawler.py
@@ -129,7 +129,7 @@ def CrawlPage(url, outfile):
 
 
 	if soup.h1 is not None: 
-		outfile.write((soup.h1.text + ":\t" + protein +  " " +  fat +  " " + carb +  " " + sugar + " ").encode('utf-8').strip())
+		outfile.write((soup.h1.text + ":\t" + protein +  " " +  fat +  " " + carb +  " " + sugar + " " + url).encode('utf-8').strip())
 		outfile.write("\n")
 
 	return True
